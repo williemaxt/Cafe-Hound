2952f42faab1f143b433956a98b2d2fe0b207995
William Thompson <williemaxt@gmail.com>
2018-03-07 22:47:11 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.sixoneonesolutions.william.cafehound;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button search;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //-------------------------------------CONNECTING BUTTON TO XML-------------------
+        search = findViewById(R.id.btn);
+
+
+        //-----------------------------------ON CLICK LISTENER---------------------------
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
